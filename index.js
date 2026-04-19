c1d027a4b2bd08d1800dc291e6c6bc933c3e947a
Ramez <ramez3ehab@gmail.com>
2026-04-19 22:21:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '249115962269', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
