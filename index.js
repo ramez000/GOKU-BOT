71202c1b7545c09a2a49b653ca88d5b211899246
Ramez <ramez3ehab@gmail.com>
2026-04-20 07:31:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Client } from 'meowsab';
+import { Client } from 'Kintoun';
 import { group, access } from "./system/control.js";
 import UltraDB from "./system/UltraDB.js";
 import sub from './sub.js';
@@ -10,13 +10,7 @@ const client = new Client({
   fromMe: false, 
   owners: [
   // Owner 1
-    { name: "VA", lid: "247579682029763@lid", jid: "201066826750@s.whatsapp.net" },
-  // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
-  // Owner 3
-    { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
-  // Owner 4 
-   { name: "عمورتي", jid: "201050079089@s.whatsapp.net", lid: "51664513925368@lid" }
+    { name: "RAMEZ", lid: "@lid", jid: " 249962416396@s.whatsapp.net" }
   ],
   settings: { noWelcome: true },
   commandsPath: './plugins'
@@ -33,22 +27,20 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = { 
-  nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
-  idChannel: "120363225356834044@newsletter",
+  nameBot: "𝐆𝐎𝐊𝐔 𝐁𝐎𝐓🐉", 
+  nameChannel: "", 
+  idChannel: "",
   urls: {
-    repo: "https://github.com/deveni0/Pomni-AI",
-    api: "https://emam-api.web.id",
-    channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
+    repo: "https://github.com/ramez000/GOKU-BOT",
+    api: "",
+    channel: ""
   },
   copyright: { 
-    pack: 'ڤـ ـ VA ـ ـا', 
-    author: 'VA'
+    pack: 'Goku bot pack', 
+    author: 'RAMEZ'
   },
   images: [
-    "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
-    "https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg",
-    "https://i.pinimg.com/originals/bb/77/0f/bb770fad66a634a6b3bf93e9c00bf4e5.jpg"
+    "https://i.imgur.com/clDbZ1w.jpeg"
   ]
 };
 
