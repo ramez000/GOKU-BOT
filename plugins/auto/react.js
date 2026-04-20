ed05a224a0f8402c05ea4465f380685e904bdc73
Ramez <ramez3ehab@gmail.com>
2026-04-20 23:43:32 +0200

    Update react.js

--- plugins/auto/react.js
+++ plugins/auto/react.js
@@ -1,7 +1,7 @@
 export default async function before(m, { conn , bot }) {
   const triggers = {
-    "ڤينوم": ["📂", "🗃️", "☕", "🪐", "🐍"],
-    "ڤيني": ["🎼", "🎪", "🎨", "🎯", "🎲"]
+    "اوامر": ["📂"],
+    "تست": ["✅"]
   };
 
   const emojis = triggers[m.text];
