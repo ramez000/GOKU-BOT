a1c42bd52864345b8a3c29b0e8e51f783e362bfe
Ramez <ramez3ehab@gmail.com>
2026-04-20 22:27:42 +0200

    Update script.js

--- plugins/group /script.js
+++ plugins/group /script.js
@@ -7,13 +7,13 @@ const context = (jid, img) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
-        newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        newsletterJid: '',
+        newsletterName: '',
         serverMessageId: 0
     },
     externalAdReply: {
-        title: "𝐏𝐎𝐌𝐍𝐈-𝐀𝐈 🎪 | 𝐁𝐨𝐭 𝐢𝐬 𝐛𝐮𝐢𝐥𝐭 𝐨𝐧 𝐭𝐡𝐞 𝐖𝐒/𝐕𝐈𝐈 𝐟𝐫𝐚𝐦𝐞𝐰𝐨𝐫𝐤",
-        body: "𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 𝚋𝚘𝚝 𝚝𝚑𝚊𝚝 𝚒𝚜 𝚎𝚊𝚜𝚢 𝚝𝚘 𝚖𝚘𝚍𝚒𝚏𝚢 𝚊𝚗𝚍 𝚟𝚎𝚛𝚢 𝚏𝚊𝚜𝚝",
+        title: "",
+        body: "",
         thumbnailUrl: img,
         sourceUrl: '',
         mediaType: 1,
@@ -24,9 +24,7 @@ const { images } = bot.config.info;
 const img = images.random()
 await conn.sendMessage(m.chat, { 
   text: `
-GitHub: _*https://github.com/deveni0/Pomni-AI/tree/main*_
-
-Video: _*https://youtu.be/hA5aCpvesJE?si=pHAEsbDFTVXe2_sq*_
+GitHub: _*https://github.com/ramez000/GOKU-BOT/tree/main*_
 
 > *لا تنسي وضع نجمة لـ الريبو 🌟*
 `,
