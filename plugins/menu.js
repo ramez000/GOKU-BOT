bf803c4641be0fbce882219abb811866d748bf4e
Ramez <ramez3ehab@gmail.com>
2026-04-22 23:21:00 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -46,7 +46,10 @@ ${CATEGORIES.map(c => `➤ ${c[0]} │ ${c[3]} ${c[1]}`).join('\n')}
 > *╰─┈─┈─⟞🐉⟝─┈─┈─╯*
 `.trim();
 
-    const msg = await conn.sendMessage(m.chat, { text });
+    const msg = await conn.sendMessage(m.chat, {
+    image: { url: 'https://i.imgur.com/clDbZ1w.jpeg' },
+    caption: ''
+});
 
     global.menus[msg.key.id] = {
         chatId: m.chat,
