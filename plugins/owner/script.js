0fc471db332c2afbeaa42971ab2c585a337b853e
Ramez <ramez3ehab@gmail.com>
2026-05-04 07:33:24 +0200

    Create script.js

---
+++ plugins/owner/script.js
@@ -0,0 +1,38 @@
+let handler = async (m, {
+    conn,
+    bot
+}) => {
+const context = (jid, img) => ({
+    mentionedJid: [jid],
+    isForwarded: true,
+    forwardingScore: 1,
+    forwardedNewsletterMessageInfo: {
+        newsletterJid: '',
+        newsletterName: '',
+        serverMessageId: 0
+    },
+    externalAdReply: {
+        title: "",
+        body: "",
+        thumbnailUrl: img,
+        sourceUrl: '',
+        mediaType: 1,
+        renderLargerThumbnail: true
+    }
+});
+const { images } = bot.config.info;
+const img = images.random()
+await conn.sendMessage(m.chat, { 
+  text: `
+GitHub: _*https://github.com/ramez000/GOKU-BOT/tree/main*_
+
+> *لا تنسى وضع نجمة للريبو 🌟*
+`,
+  contextInfo: context(m.sender, img)
+}, { quoted: reply_status });
+}
+handler.usage = ["سكريبت"];
+handler.category = "group";
+handler.command = ["سكريبت", "سورس", "sc"];
+
+export default handler;
