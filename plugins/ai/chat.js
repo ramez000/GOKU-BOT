670d29a5b86cc73b0ffbb741918af6a1419fc15a
Ramez <ramez3ehab@gmail.com>
2026-07-03 09:23:03 +0200

    Create chat.js

---
+++ plugins/ai/chat.js
@@ -0,0 +1,13 @@
+import { AiChat } from "../../system/utils.js";
+
+const handler = async (m, { conn, text, bot }) => {
+  if (!text) return m.reply("💙 ~ أكتب نص مع الأمر ~ ❤️");
+  const res = await AiChat({ text });
+  m.reply(res);
+};
+
+handler.usage = ["بوت"];
+handler.category = "ai";
+handler.command = ["بوت"];
+
+export default handler;
