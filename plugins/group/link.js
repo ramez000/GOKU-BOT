aca37b7a8402ca6a3557f25b179db9c617ed85f5
Ramez <ramez3ehab@gmail.com>
2026-07-07 00:44:24 +0200

    Update link.js

--- plugins/group/link.js
+++ plugins/group/link.js
@@ -2,26 +2,25 @@ let handler = async (m, {
     conn
 }) => {
     try {
-        m.reply(`❀° ┄──────────╮
-🌷┊ *رابـــط الـــمـــجـــمـــوعـــة:* ${(await conn.groupMetadata(m.chat)).subject}
-🌷┊
-🌷┊ https://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `
-🌷┊
-🌷┊ ${conn.user.name || "Bot WhatsApp"}
-╰──────────┄ °❀`)
+        m.reply(`𖤓° ┄─────────╮
+🐲┊ *رابـــط الـــمـــجـــمـــوعـــة:* ${(await conn.groupMetadata(m.chat)).subject}
+🐉┊
+🐲┊ https://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `
+🐉┊
+╰─────────┄ °𖤓`)
     } catch {
         const mentionedUser = conn.user.id.split(":")[0] + "@s.whatsapp.net";
         conn.sendMessage(m.chat, { 
-            text: `❀° ┄──────────╮
-🌷┊ يـــجـــب تـــعـــيـــيـــن @${mentionedUser.split('@')[0]} كـــمـــشـــرف لـــلـــتـــمـــكـــن مـــن اســـتـــخـــدام هـــذا الـــأمـــر!
-╰──────────┄ °❀`, 
+            text: `𖤓° ┄──────────╮
+🐲┊ يـــجـــب تـــعـــيـــيـــن @${mentionedUser.split('@')[0]} كـــمـــشـــرف لـــلـــتـــمـــكـــن مـــن اســـتـــخـــدام هـــذا الـــأمـــر!
+╰──────────┄ °𖤓`, 
             mentions: [mentionedUser]
         })
     }
 }
 handler.usage = ["لينك"];
 handler.category = "group";
-handler.command = ["لينك", "link"];
+handler.command = ["لينك", "link", "رابط" ,"الرابط"];
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
