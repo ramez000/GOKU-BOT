aaffea43c98d4aa2f124cc4547e1cbab21a867a9
Ramez <ramez3ehab@gmail.com>
2026-07-07 10:06:32 +0200

    Create stop.js

---
+++ plugins/owner/stop.js
@@ -0,0 +1,19 @@
+const test = async (m, { conn, bot }) => {
+  m.react("🟢")
+  
+  conn.msgUrl(m.chat, "🐉 Stopping the bot...", { 
+    title: "",
+    body: "",
+    img: "https://i.imgur.com/faYfciD.jpeg",
+    big: false 
+  });
+  
+  setTimeout(() => {
+    bot.stop();
+  }, 1000); 
+};
+
+test.category = "owner";
+test.command = ["ايقاف", "stop"];
+test.owner = true;
+export default test;
